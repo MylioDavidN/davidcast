9ac7036f264f7d4fc0051de0c2156f6d478278bf
David T. Nguyen <davidnguyen@fb.com>
2016-08-28 17:41:57 -0700

    all files

---
+++ src/com/google/sample/cast/refplayer/CastOptionsProvider.java
@@ -0,0 +1,36 @@
+package com.google.sample.cast.refplayer;
+
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
+
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by davidnguyen on 8/28/16.
+ */
+public class CastOptionsProvider implements OptionsProvider{
+    @Override
+    public CastOptions getCastOptions(Context context) {
+        NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setTargetActivityClassName(VideoBrowserActivity.class.getName())
+                .build();
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .build();
+
+        return new CastOptions.Builder()
+                .setReceiverApplicationId(context.getString(R.string.app_id))
+                .setCastMediaOptions(mediaOptions)
+                .build();
+    }
+
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
+}
